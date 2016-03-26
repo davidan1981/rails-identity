5570aadae4d3be052845bf5dc46a8bdd1b0de1cd
David An <davidan1981@gmail.com>
2016-03-25 22:44:57 -0500

    Remove irrelevant old code

--- app/models/rails_identity/user.rb
+++ app/models/rails_identity/user.rb
@@ -2,11 +2,8 @@ module RailsIdentity
   class User < ActiveRecord::Base
     include UUIDModel
     acts_as_paranoid
-
-    has_one :cart, foreign_key: "user_uuid", primary_key: "uuid"
-    has_many :orders, foreign_key: "user_uuid", primary_key: "uuid"
-
     has_secure_password
+
     validates :username, presence: true, uniqueness: true,
               format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     validates :password, confirmation: true
