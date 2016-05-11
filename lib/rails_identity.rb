cefae1752c2848cab092318fafe6e6d4e1e1e5ce
David An <davidan1981@gmail.com>
2016-05-10 19:58:52 -0500

    Refactor roles

--- lib/rails_identity.rb
+++ lib/rails_identity.rb
@@ -1,17 +1,10 @@
 require "rails_identity/engine"
 require "rails_identity/cache"
+require "rails_identity/roles"
 
 module RailsIdentity
 
   # App MUST monkey patch this constant
   MAILER_EMAIL = "no-reply@rails-identity.com"
-
-  # Fixed set of roles.
-  module Roles
-    PUBLIC = 0
-    USER = 10
-    ADMIN = 100
-    OWNER = 1000
-  end
 
 end
